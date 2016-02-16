6ff46d93158595be81a62353d705be42f1683acc
Don <don.j.olmstead@gmail.com>
2016-02-16 00:01:02 -0800

    Add testing and coverage to the build

--- build.sh
+++ build.sh
@@ -6,3 +6,23 @@ dart --version
 
 # Get dependencies
 pub install
+
+# Run the tests
+pub global activate coverage
+OBSERVATORY_PORT=8000
+COVERAGE_OUTPUT=coverage.json
+
+dart --checked --observe=${OBSERVATORY_PORT} test/all.dart & \
+pub global run coverage:collect_coverage \
+    --port=${OBSERVATORY_PORT} \
+    --out ${COVERAGE_OUTPUT} \
+    --wait-paused \
+    --resume-isolates & \
+wait
+
+pub global run coverage:format_coverage \
+    --package-root=packages \
+    --report-on lib \
+    --in ${COVERAGE_OUTPUT} \
+    --out lcov.info \
+    --lcov
