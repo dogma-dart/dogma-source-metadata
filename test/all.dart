4478bdf1eca3525fd08cedac2fe4369cc8fcd334
Don <don.j.olmstead@gmail.com>
2016-02-16 00:09:15 -0800

    Add type metadata tests

--- test/all.dart
+++ test/all.dart
@@ -12,13 +12,17 @@
 import 'package:test/test.dart';
 
 import 'src/analyzer/field_metadata_test.dart' as field_metadata_test;
+import 'src/metadata/type_metadata_test.dart' as type_metadata_test;
 
 //---------------------------------------------------------------------
 // Library contents
 //---------------------------------------------------------------------
 
 void main() {
-  group('Analyzer tests', (){
+  group('Analyzer tests', () {
     group('FieldMetadata', field_metadata_test.main);
+  });
+  group('Metadata tests', () {
+    group('TypeMetadata', type_metadata_test.main);
   });
 }
