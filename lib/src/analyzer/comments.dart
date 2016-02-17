bab8ac7b75895cb5c001c25b196fc76fe5785fe2
Don <don.j.olmstead@gmail.com>
2016-02-16 22:58:28 -0800

    Add ability to get document comments

---
+++ lib/src/analyzer/comments.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:analyzer/src/generated/element.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Retrieves the documentation comments from the [element].
+///
+/// If no documentation comments are found then the empty string will be
+/// returned.
+String elementComments(Element element) =>
+    element.computeDocumentationComment() ?? '';
