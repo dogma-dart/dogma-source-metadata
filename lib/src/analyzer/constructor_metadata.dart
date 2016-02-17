c212522099c2b5782a3fcdc988057c73b8f465d8
Don <don.j.olmstead@gmail.com>
2016-02-16 22:58:45 -0800

    Add constructor metadata creation

---
+++ lib/src/analyzer/constructor_metadata.dart
@@ -0,0 +1,62 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:analyzer/src/generated/element.dart';
+import 'package:logging/logging.dart';
+
+import '../../metadata.dart';
+import 'annotation.dart';
+import 'comments.dart';
+import 'parameter_metadata.dart';
+import 'type_metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// The logger for the library.
+final Logger _logger =
+    new Logger('dogma_source_analyzer.src.analyzer.constructor_metadata');
+
+/// Creates metadata for the given constructor [element].
+ConstructorMetadata constructorMetadata(ConstructorElement element) {
+  var annotations = createAnnotations(element, []);
+  var comments = elementComments(element);
+
+  var name = element.name;
+  var parameters = parameterList(element);
+  var isPrivate = element.isPrivate;
+  var isFactory = element.isFactory;
+  var returnType = new TypeMetadata('Foo');
+
+  if (name.isEmpty) {
+    _logger.fine('Found default constructor');
+
+    return new ConstructorMetadata(
+        returnType,
+        parameters: parameters,
+        isPrivate: isPrivate,
+        isFactory: isFactory,
+        annotations: annotations,
+        comments: comments
+    );
+  } else {
+    _logger.fine('Found named constructor $name');
+
+    return new ConstructorMetadata.named(
+        name,
+        returnType,
+        parameters: parameters,
+        isPrivate: isPrivate,
+        isFactory: isFactory,
+        annotations: annotations,
+        comments: comments
+    );
+  }
+}
