352ffa2beb02118a0d8ff55ddeeb14bf63d2ef55
Don <don.j.olmstead@gmail.com>
2016-02-17 01:10:44 -0800

    Add function metadata test

--- test/all.dart
+++ test/all.dart
@@ -14,6 +14,7 @@ import 'package:logging/logging.dart';
 
 import 'src/analyzer/constructor_metadata_test.dart' as constructor_metadata_test;
 import 'src/analyzer/field_metadata_test.dart' as field_metadata_test;
+import 'src/analyzer/function_metadata_test.dart' as function_metadata_test;
 
 import 'src/metadata/parameter_metadata_test.dart' as parameter_metadata_test;
 import 'src/metadata/type_metadata_test.dart' as type_metadata_test;
@@ -33,6 +34,7 @@ void main() {
   group('Analyzer tests', () {
     group('ConstructorMetadata', constructor_metadata_test.main);
     group('FieldMetadata', field_metadata_test.main);
+    group('FunctionMetadata', function_metadata_test.main);
   });
 
   // Run metadata test
