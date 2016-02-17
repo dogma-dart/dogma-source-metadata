25693aa127b1f939f6eeac3c0dd5bb71a8497bb0
Don <don.j.olmstead@gmail.com>
2016-02-16 23:01:44 -0800

    Adding more metadata to classes

--- lib/src/analyzer/class_metadata.dart
+++ lib/src/analyzer/class_metadata.dart
@@ -11,7 +11,10 @@ import 'package:analyzer/src/generated/element.dart';
 import 'package:logging/logging.dart';
 
 import '../../metadata.dart';
+import 'comments.dart';
+import 'constructor_metadata.dart';
 import 'field_metadata.dart';
+import 'type_metadata.dart';
 
 //---------------------------------------------------------------------
 // Library contents
@@ -25,32 +28,76 @@ ClassMetadata classMetadata(ClassElement element) {
   var name = element.name;
   _logger.info('Creating metadata for class $name');
 
+  // Get the supertype
+  var supertypeElement = element.supertype;
+  var supertypeName = supertypeElement.name;
+  var supertype;
+
+  if (supertypeName != 'Object') {
+    _logger.fine('Found that $name extends $supertypeName');
+  } else {
+    _logger.fine('Found that $name extends Object');
+    supertype = typeMetadata(supertypeElement);
+  }
+
+  // Get the classes that are mixed in
+  var mixins = <TypeMetadata>[];
+
+  for (var mixin in element.mixins) {
+    var mixinType = typeMetadata(mixin);
+
+    _logger.fine('Found that $name mixes in ${mixinType.name}');
+
+    mixins.add(mixinType);
+  }
+
+  // Get the interfaces the class implements
+  var interfaces = <TypeMetadata>[];
+
+  for (var interface in element.interfaces) {
+    var interfaceType = typeMetadata(interface);
+
+    _logger.fine('Found that $name implements ${interfaceType.name}');
+
+    interfaces.add(interfaceType);
+  }
+
+  // Get the fields
   var fields = <FieldMetadata>[];
 
-  // Iterate over the fields
   for (var field in element.fields) {
     var fieldName = field.name;
     _logger.fine('Found field $fieldName on $name');
     fields.add(fieldMetadata(field));
   }
 
-  var supertypeName = element.supertype.name;
+  // Get the methods
+  var methods = <MethodMetadata>[];
 
-  if (supertypeName != 'Object') {
-    _logger.fine('Found that $name extends $supertypeName');
-  } else {
-    _logger.fine('Found that $name extends Object');
+  for (var method in element.methods) {
+    var methodName = method.name;
+    _logger.fine('Found method $methodName on $name');
   }
 
-  for (var mixin in element.mixins) {
-    var mixinName = mixin.name;
-    _logger.fine('Found that $name mixes in $mixinName');
+  // Get the constructors
+  var constructors = <ConstructorMetadata>[];
+
+  for (var constructor in element.constructors) {
+    constructors.add(constructorMetadata(constructor));
   }
 
-  for (var interface in element.interfaces) {
-    var interfaceName = interface.name;
-    _logger.fine('Found that $name implements $interfaceName');
-  }
+  // Get the annotations
+  var annotations = [];
 
-  return new ClassMetadata(name, fields: fields);
+  return new ClassMetadata(
+      name,
+      supertype: supertype,
+      interfaces: interfaces,
+      mixins: mixins,
+      fields: fields,
+      methods: methods,
+      constructors: constructors,
+      annotations: annotations,
+      comments: elementComments(element)
+  );
 }
