d8f298491b5b3d0967b5147998875a5261b7a34b
Don <don.j.olmstead@gmail.com>
2016-02-16 22:55:23 -0800

    Add metadata for privacy

---
+++ lib/src/metadata/privacy_metadata.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Contains metadata for a method.
+abstract class PrivacyMetadata {
+  /// Whether the metadata is private to the library.
+  bool get isPrivate;
+
+  /// Whether the metadata is public to the library.
+  bool get isPublic => !isPrivate;
+}
