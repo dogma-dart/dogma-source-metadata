c6a04e2c244b8e03919695d7ca0c6cc09815f684
Don <don.j.olmstead@gmail.com>
2016-02-19 22:46:04 -0800

    Adding tests for constructor metadata construction

---
+++ test/src/metadata/constructor_metadata_test.dart
@@ -0,0 +1,51 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/metadata.dart';
+
+import 'base_metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Test entry point.
+void main() {
+  test('default constructor', () {
+    var returnType = new TypeMetadata('Foo');
+    var metadata = new ConstructorMetadata(returnType);
+
+    // Base classes
+    expectAnnotatedMetadataDefaults(metadata);
+    expectPrivacyMetadataDefaults(metadata);
+    expectFunctionMetadataDefaults(metadata);
+
+    // FunctionMetadata
+    expect(metadata.returnType, returnType);
+    expect(metadata.name, '');
+    expect(metadata.isDefault, true);
+  });
+  test('named constructor', () {
+    var constructorName = 'bar';
+    var returnType = new TypeMetadata('Foo');
+    var metadata = new ConstructorMetadata.named(constructorName, returnType);
+
+    // Base classes
+    expectAnnotatedMetadataDefaults(metadata);
+    expectPrivacyMetadataDefaults(metadata);
+    expectFunctionMetadataDefaults(metadata);
+
+    // FunctionMetadata
+    expect(metadata.returnType, returnType);
+    expect(metadata.name, constructorName);
+    expect(metadata.isDefault, false);
+  });
+}
