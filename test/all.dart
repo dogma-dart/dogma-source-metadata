5c2f1a5b89966fe6dee68a4e196f359ced92ae61
Don <don.j.olmstead@gmail.com>
2016-02-19 22:48:04 -0800

    Hooking in test runners

--- test/all.dart
+++ test/all.dart
@@ -12,12 +12,8 @@
 import 'package:test/test.dart';
 import 'package:logging/logging.dart';
 
-import 'src/analyzer/constructor_metadata_test.dart' as constructor_metadata_test;
-import 'src/analyzer/field_metadata_test.dart' as field_metadata_test;
-import 'src/analyzer/function_metadata_test.dart' as function_metadata_test;
-
-import 'src/metadata/parameter_metadata_test.dart' as parameter_metadata_test;
-import 'src/metadata/type_metadata_test.dart' as type_metadata_test;
+import 'analyzer.dart' as analyzer;
+import 'metadata.dart' as metadata;
 
 //---------------------------------------------------------------------
 // Library contents
@@ -30,16 +26,7 @@ void main() {
     print('${rec.level.name}: ${rec.time}: ${rec.message}');
   });
 
-  // Run analyzer tests
-  group('Analyzer tests', () {
-    group('ConstructorMetadata', constructor_metadata_test.main);
-    group('FieldMetadata', field_metadata_test.main);
-    group('FunctionMetadata', function_metadata_test.main);
-  });
-
-  // Run metadata test
-  group('Metadata tests', () {
-    group('ParameterMetadata', parameter_metadata_test.main);
-    group('TypeMetadata', type_metadata_test.main);
-  });
+  // Run tests
+  group('Analyzer tests', analyzer.main);
+  group('Metadata tests', metadata.main);
 }
