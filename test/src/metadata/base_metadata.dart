d05e4742dbf664599b953c0168c7a3aab49ede2d
Don <don.j.olmstead@gmail.com>
2016-02-19 22:45:27 -0800

    Adding expectations for base metadata classes

---
+++ test/src/metadata/base_metadata.dart
@@ -0,0 +1,43 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Checks the [metadata] for default values of [AbstractMetadata].
+void expectAbstractMetadataDefaults(AbstractMetadata metadata) {
+  expect(metadata.isAbstract, false);
+}
+
+/// Checks the [metadata] for default values of [AnnotatedMetadata].
+void expectAnnotatedMetadataDefaults(AnnotatedMetadata metadata) {
+  expect(metadata.annotations, isEmpty);
+  expect(metadata.comments, isEmpty);
+}
+
+/// Checks the [metadata] for default values of [PrivacyMetadata].
+void expectPrivacyMetadataDefaults(PrivacyMetadata metadata) {
+  expect(metadata.isPrivate, false);
+  expect(metadata.isPublic, true);
+}
+
+/// Checks the [metadata] for default values of [StaticMetadata].
+void expectStaticMetadataDefaults(StaticMetadata metadata) {
+  expect(metadata.isStatic, false);
+}
+
+/// Checks the [metadata] for default values of [FunctionMetadata].
+void expectFunctionMetadataDefaults(FunctionMetadata metadata) {
+  expect(metadata.parameters, isEmpty);
+}
