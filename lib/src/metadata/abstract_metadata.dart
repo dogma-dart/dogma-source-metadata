023c8c6364dc53e5eb73036536e0fd8e35448cee
Don <don.j.olmstead@gmail.com>
2016-02-19 22:42:49 -0800

    Adding abstract metadata interface

---
+++ lib/src/metadata/abstract_metadata.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// An interface for metadata that can be abstract.
+abstract class AbstractMetadata {
+  /// Whether the metadata is abstract.
+  bool get isAbstract;
+}
