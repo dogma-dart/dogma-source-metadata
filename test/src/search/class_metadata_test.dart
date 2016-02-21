f6b4498352dc4061ca3251bcfcc0da97cd2d0d30
Don <don.j.olmstead@gmail.com>
2016-02-21 08:46:57 -0800

    Add class search tests

---
+++ test/src/search/class_metadata_test.dart
@@ -0,0 +1,57 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/analyzer.dart';
+import 'package:dogma_source_analyzer/metadata.dart';
+import 'package:dogma_source_analyzer/path.dart';
+import 'package:dogma_source_analyzer/search.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+void main() {
+  var context = analysisContext();
+  var fieldLibrary = libraryMetadata(join('test/lib/fields.dart'), context);
+
+  test('field by name tests', () {
+
+  });
+  test('static field tests', () {
+    var clazz = metadataByNameQuery/*<ClassMetadata>*/(
+        fieldLibrary,
+        'ClassFields',
+        includeClasses: true
+    );
+    expect(clazz, isNotNull);
+
+    var staticFields = classStaticFieldQueryAll/*<FieldMetadata>*/(clazz);
+    for (var field in staticFields) {
+      expect(field.isStatic, true);
+    }
+    expect(staticFields.length, 6);
+
+    var staticConstFields = classMetadataQueryAll/*<FieldMetadata>*/(
+        clazz,
+        and(staticMatch, constFieldMatch),
+        includeFields: true
+    );
+    expect(staticConstFields.length, 1);
+    var staticConstField = staticConstFields.first as FieldMetadata;
+    expect(staticConstField.name, 'classConstField');
+    expect(staticConstField.isConst, true);
+    expect(staticConstField.isStatic, true);
+    expect(staticConstField.isFinal, false);
+  });
+  test('instance field tests', () {
+
+  });
+}
