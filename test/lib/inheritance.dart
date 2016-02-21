f508f1b70b4927b03bf7df6a3f7f2bf073ff1537
Don <don.j.olmstead@gmail.com>
2016-02-21 09:28:00 -0800

    Add test file for inheritance

---
+++ test/lib/inheritance.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+class A {
+  int a;
+}
+
+class B extends A {
+  int b;
+}
+
+class C extends B {
+  int c;
+}
+
+class D extends C {
+  int d;
+}
