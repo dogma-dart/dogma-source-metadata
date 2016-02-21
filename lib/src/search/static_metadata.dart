8114edcc702a8bf881e251cea7658d156d3269c4
Don <don.j.olmstead@gmail.com>
2016-02-21 08:43:08 -0800

    Add matcher functions for static metadata

---
+++ lib/src/search/static_metadata.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import '../../metadata.dart';
+import 'metadata_match_function.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Matches [metadata] that is an instance of [StaticMetadata].
+bool staticMetadataMatch(Metadata metadata) => metadata is AbstractMetadata;
+
+/// Matches [metadata] that is statically scoped.
+///
+/// The function assumes that [metadata] is [StaticMetadata]. For cases where
+/// the metadata being queried can return instances of [Metadata] that are not
+/// [StaticMetadata] then this function should be joined with
+/// staticMetadataMatch.
+///
+///     and(staticMetadataMatch, staticMatch);
+bool staticMatch(Metadata metadata) => (metadata as StaticMetadata).isStatic;
+
+/// Matches [metadata] that is scoped to an instance.
+///
+/// The function assumes that [metadata] is [StaticMetadata]. For cases where
+/// the metadata being queried can return instances of [Metadata] that are not
+/// [StaticMetadata] then this function should be joined with
+/// staticMetadataMatch.
+///
+///     and(abstractMetadataMatch, instanceMatch);
+final MetadataMatchFunction instanceMatch = not(staticMatch);
