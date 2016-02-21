0eb2d6051f5d213cf72df7556816eaf925c0b933
Don <don.j.olmstead@gmail.com>
2016-02-21 08:47:08 -0800

    Add search test grouping

---
+++ test/search.dart
@@ -0,0 +1,22 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+// \TODO Remove this file after https://github.com/dart-lang/test/issues/36 is resolved.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+
+import 'src/search/class_metadata_test.dart' as class_metadata;
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+void main() {
+  group('ClassMetadata', class_metadata.main);
+}
