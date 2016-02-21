4ae6a2852c97a4d2be1cc6a1aa760a6221f35227
Don <don.j.olmstead@gmail.com>
2016-02-21 09:28:13 -0800

    Add test file for mixins

---
+++ test/lib/mixins.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+class A {
+  int a;
+}
+
+class B {
+  int b;
+}
+
+class C {
+  int c;
+}
+
+class D extends A with B, C {
+  int d;
+}
