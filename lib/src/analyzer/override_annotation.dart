2f63d9ed455af137840c037767b1abbedef866b5
Don <don.j.olmstead@gmail.com>
2016-02-24 22:19:57 -0800

    Create function for instantiating override annotations

---
+++ lib/src/analyzer/override_annotation.dart
@@ -0,0 +1,30 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:analyzer/src/generated/element.dart';
+import 'package:logging/logging.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// The logger for the library.
+final Logger _logger =
+    new Logger('dogma_source_analyzer.src.analyzer.override_annotation');
+
+/// Determines if the annotation corresponds to an @override annotation.
+dynamic analyzeOverrideAnnotation(ElementAnnotationImpl element) {
+  if (element.isOverride) {
+    _logger.fine('Found override annotation');
+
+    return override;
+  } else {
+    return null;
+  }
+}
