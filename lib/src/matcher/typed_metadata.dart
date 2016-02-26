8e178ab41b46cba4eed0df79b12f37c37fcf34ff
Don <don.j.olmstead@gmail.com>
2016-02-25 23:50:30 -0800

    Create matchers for typed metadata

--- lib/src/matcher/typed_metadata.dart
+++ lib/src/matcher/typed_metadata.dart
@@ -14,6 +14,13 @@ import 'metadata_match_function.dart';
 // Library contents
 //---------------------------------------------------------------------
 
-/// Matches metadata based on the given [name].
-MetadataMatchFunction nameMatch(String name) =>
-    (metadata) => metadata.name == name;
+/// Matches [metadata] that is an instance of [TypedMetadata].
+bool typedMetadataMatch(Metadata metadata) => metadata is TypedMetadata;
+
+/// Matches typed metadata of the given [type].
+MetadataMatchFunction typeMatch(TypeMetadata type) =>
+    (metadata) => (metadata as TypedMetadata).type == type;
+
+/// Matched typed [metadata] whose type is builtin.
+bool builtinTypeMatch(Metadata metadata) =>
+    (metadata as TypedMetadata).type.isBuiltin;
