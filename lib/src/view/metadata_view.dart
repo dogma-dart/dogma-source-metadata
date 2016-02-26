92139caaa76547e27f7023457970e6d1d93ea448
Don <don.j.olmstead@gmail.com>
2016-02-25 23:26:05 -0800

    Add view library

---
+++ lib/src/view/metadata_view.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import '../../metadata.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+abstract class MetadataView<T extends Metadata> {
+  final T metadata;
+
+  MetadataView(this.metadata);
+}
---
+++ lib/view.dart
@@ -0,0 +1,14 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library dogma_source_analyzer.view;
+
+// \TODO Should this be in dogma_source_analyzer??
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/view/metadata_view.dart';
