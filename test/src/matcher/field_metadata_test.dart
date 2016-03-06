ead758c5ab1e333cd742d549198ee0acc6752220
Don <don.j.olmstead@gmail.com>
2016-03-06 09:36:12 -0800

    Add field metadata matcher tests

--- test/src/matcher/field_metadata_test.dart
+++ test/src/matcher/field_metadata_test.dart
@@ -24,7 +24,81 @@ final FieldMetadata _instanceField =
         isConst: false,
         isFinal: false
     );
+final FieldMetadata _instanceFinalField =
+    new FieldMetadata.field(
+        'instanceField',
+        _fieldType,
+        isConst: false,
+        isFinal: true
+    );
+final FieldMetadata _instanceGetter =
+    new FieldMetadata(
+        'instanceGetter',
+        _fieldType,
+        true,
+        true,
+        false,
+        isConst: false,
+        isFinal: false
+    );
+final FieldMetadata _instanceSetter =
+    new FieldMetadata(
+        'instanceSetter',
+        _fieldType,
+        true,
+        false,
+        true,
+        isConst: false,
+        isFinal: false
+    );
+final Metadata _notFieldMetadata = new Metadata('NotField');
 
 void main() {
-
+  test('fieldMetadataMatch', () {
+    expect(fieldMetadataMatch(_instanceField), isTrue);
+    expect(fieldMetadataMatch(_instanceFinalField), isTrue);
+    expect(fieldMetadataMatch(_instanceGetter), isTrue);
+    expect(fieldMetadataMatch(_instanceSetter), isTrue);
+    expect(fieldMetadataMatch(_notFieldMetadata), isFalse);
+  });
+  test('finalFieldMatch', () {
+    expect(finalFieldMatch(_instanceField), isFalse);
+    expect(finalFieldMatch(_instanceFinalField), isTrue);
+    expect(finalFieldMatch(_instanceGetter), isFalse);
+    expect(finalFieldMatch(_instanceSetter), isFalse);
+    expect(() => finalFieldMatch(_notFieldMetadata), throws);
+    expect(and(fieldMetadataMatch, finalFieldMatch)(_notFieldMetadata), isFalse);
+  });
+  test('propertyFieldMatch', () {
+    expect(propertyFieldMatch(_instanceField), isFalse);
+    expect(propertyFieldMatch(_instanceFinalField), isFalse);
+    expect(propertyFieldMatch(_instanceGetter), isTrue);
+    expect(propertyFieldMatch(_instanceSetter), isTrue);
+    expect(() => propertyFieldMatch(_notFieldMetadata), throws);
+    expect(and(fieldMetadataMatch, propertyFieldMatch)(_notFieldMetadata), isFalse);
+  });
+  test('getterFieldMatch', () {
+    expect(getterFieldMatch(_instanceField), isTrue);
+    expect(getterFieldMatch(_instanceFinalField), isTrue);
+    expect(getterFieldMatch(_instanceGetter), isTrue);
+    expect(getterFieldMatch(_instanceSetter), isFalse);
+    expect(() => getterFieldMatch(_notFieldMetadata), throws);
+    expect(and(fieldMetadataMatch, getterFieldMatch)(_notFieldMetadata), isFalse);
+  });
+  test('setterFieldMatch', () {
+    expect(setterFieldMatch(_instanceField), isTrue);
+    expect(setterFieldMatch(_instanceFinalField), isFalse);
+    expect(setterFieldMatch(_instanceGetter), isFalse);
+    expect(setterFieldMatch(_instanceSetter), isTrue);
+    expect(() => setterFieldMatch(_notFieldMetadata), throws);
+    expect(and(fieldMetadataMatch, setterFieldMatch)(_notFieldMetadata), isFalse);
+  });
+  test('fieldMatch', () {
+    expect(fieldMatch(_instanceField), isTrue);
+    expect(fieldMatch(_instanceFinalField), isTrue);
+    expect(fieldMatch(_instanceGetter), isFalse);
+    expect(fieldMatch(_instanceSetter), isFalse);
+    expect(() => fieldMatch(_notFieldMetadata), throws);
+    expect(and(fieldMetadataMatch, fieldMatch)(_notFieldMetadata), isFalse);
+  });
 }
