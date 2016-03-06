8ffecc8690a34ac3530038fd83d1290999ea7ac4
Don <don.j.olmstead@gmail.com>
2016-03-06 08:45:08 -0800

    Modify matcher name

--- test/src/search/class_metadata_test.dart
+++ test/src/search/class_metadata_test.dart
@@ -46,7 +46,7 @@ void main() {
 
     var staticConstFields = classMetadataQueryAll/*<FieldMetadata>*/(
         clazz,
-        and(staticMatch, constFieldMatch),
+        and(staticMatch, constantMatch),
         includeFields: true
     );
     expect(staticConstFields, hasLength(1));
