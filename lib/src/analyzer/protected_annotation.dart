fd0a265d7225395ece9694edc086e5137b43257c
Don <don.j.olmstead@gmail.com>
2016-03-06 08:42:34 -0800

    Add analyzer support for protected metadata

---
+++ lib/src/analyzer/protected_annotation.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:analyzer/src/generated/element.dart';
+import 'package:logging/logging.dart';
+import 'package:meta/meta.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// The logger for the library.
+final Logger _logger =
+    new Logger('dogma_source_analyzer.src.analyzer.protected_annotation');
+
+/// Determines if the annotation corresponds to a @protected annotation.
+dynamic analyzeProtectedAnnotation(ElementAnnotationImpl element) {
+  if (element.isProtected) {
+    _logger.fine('Found protected annotation');
+
+    return protected;
+  } else {
+    return null;
+  }
+}
