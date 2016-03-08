18bcae8c7c20ed5405f37948e3c75728550ae857
Don <don.j.olmstead@gmail.com>
2016-03-07 22:26:32 -0800

    Add new matcher tests to runner

--- test/matcher.dart
+++ test/matcher.dart
@@ -13,10 +13,16 @@ import 'package:test/test.dart';
 
 import 'src/matcher/abstract_metadata_test.dart' as abstract_metadata_test;
 import 'src/matcher/class_metadata_test.dart' as class_metadata_test;
+import 'src/matcher/constant_metadata_test.dart' as constant_metadata_test;
 import 'src/matcher/constructor_metadata_test.dart' as constructor_metadata_test;
 import 'src/matcher/field_metadata_test.dart' as field_metadata_test;
+import 'src/matcher/function_metadata_test.dart' as function_metadata_test;
+import 'src/matcher/metadata_match_function_test.dart' as metadata_match_function_test;
+import 'src/matcher/metadata_test.dart' as metadata_test;
+import 'src/matcher/parameter_metadata_test.dart' as parameter_metadata_test;
 import 'src/matcher/privacy_metadata_test.dart' as privacy_metadata_test;
 import 'src/matcher/static_metadata_test.dart' as static_metadata_test;
+import 'src/matcher/typed_metadata_test.dart' as typed_metadata_test;
 
 //---------------------------------------------------------------------
 // Library contents
@@ -25,8 +31,14 @@ import 'src/matcher/static_metadata_test.dart' as static_metadata_test;
 void main() {
   group('AbstractMetadata', abstract_metadata_test.main);
   group('ClassMetadata', class_metadata_test.main);
+  group('ConstantMetadata', constant_metadata_test.main);
   group('ConstructorMetadata', constructor_metadata_test.main);
   group('FieldMetadata', field_metadata_test.main);
+  group('FunctionMetadata', function_metadata_test.main);
+  group('Metadata', metadata_test.main);
+  group('MetadataMatchFunction', metadata_match_function_test.main);
+  group('ParameterMetadata', parameter_metadata_test.main);
   group('PrivacyMetadata', privacy_metadata_test.main);
-  group('StaticMetadataTest', static_metadata_test.main);
+  group('StaticMetadata', static_metadata_test.main);
+  group('TypedMetadata', typed_metadata_test.main);
 }
