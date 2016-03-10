2662987a3924048abc203d2ff0a418cda5e39e3f
Don <don.j.olmstead@gmail.com>
2016-03-09 23:01:15 -0800

    Add a way to map parameters when creating an annotation

--- lib/src/analyzer/annotation.dart
+++ lib/src/analyzer/annotation.dart
@@ -14,12 +14,18 @@ import 'dart:mirrors';
 //---------------------------------------------------------------------
 
 import 'package:analyzer/src/generated/element.dart';
+import 'package:analyzer/src/generated/utilities_dart.dart';
+import 'package:logging/logging.dart';
 
 import 'constant_object.dart';
 
 //---------------------------------------------------------------------
 // Library contents
 //---------------------------------------------------------------------
+
+/// The logger for the library.
+final Logger _logger =
+    new Logger('dogma_source_analyzer.src.analyzer.annotation');
 
 /// A function that takes a annotation [element] from the analyzer and returns
 /// an instantiation of the metadata.
@@ -33,6 +39,43 @@ typedef dynamic CreateAnnotationInstance(ClassMirror mirror,
                                          Symbol constructor,
                                          List positionalArguments,
                                          Map<Symbol, dynamic> namedArguments);
+
+/// Maps the name of the [parameter] to the computed value of the instance
+/// based on the [constructor].
+///
+/// When instantiating the value of a constant object the analyzer returns the
+/// structure of the final object.
+///
+///     class Annotation {
+///       final int field;
+///
+///       Annotation(this.field);
+///     }
+///
+/// In this case the `field` passed into the constructor directly corresponds
+/// to the value of object the analyzer computes. This is true for any values
+/// created using the `this.*` syntax.
+///
+/// However the API may not have this 1-1 relationship.
+///
+///      class Annotation {
+///        final int field;
+///        final int otherField;
+///
+///         Annotation(int value, this.otherField)
+///            : field = value;
+///      }
+///
+/// In this case to construct an instance of the annotation a mapping needs to
+/// be provided. An example implementation in this case would be.
+///
+///     String mapAnnotationParameter(String constructor, String parameter) =>
+///         parameter == 'value' ? 'field' : parameter;
+///
+/// A mapping function is required for any annotations where the names of the
+/// parameters do not correspond to the names of the fields within the
+/// constructed object.
+typedef String ParameterNameMapper(String constructorName, String parameter);
 
 List createAnnotations(Element element,
                        List<AnalyzeAnnotation> annotationCreators) {
@@ -53,6 +96,7 @@ List createAnnotations(Element element,
 
 AnalyzeAnnotation analyze(String annotation,
                          {String library: '',
+                          ParameterNameMapper parameterNameMapper: _passThroughParameters,
                           CreateAnnotationInstance createAnnotation: _createAnnotation}) {
   var clazz = classMirror(annotation, library);
 
@@ -67,6 +111,9 @@ AnalyzeAnnotation analyze(String annotation,
     var value;
 
     if (representation is ConstructorElement) {
+      var constructorName = representation.name;
+      _logger.fine('Annotation is being constructed through "$constructorName"');
+
       // Annotations are constant so get the result of the evaluation
       //
       // This ends up creating a generic object containing the resulting
@@ -80,22 +127,30 @@ AnalyzeAnnotation analyze(String annotation,
       // Iterate over the parameters to get the mirrors call
       for (var parameter in representation.parameters) {
         var parameterName = parameter.name;
-        var parameterField = evaluatedFields[parameterName];
+        var mappedParameterName = parameterNameMapper(constructorName, parameterName);
+        var parameterField = evaluatedFields[mappedParameterName];
         var parameterValue = dartValue(parameterField);
 
-        if (parameter.parameterKind == 'NAMED') {
+        _logger.fine('Found $parameterName of value $parameterValue');
+        _logger.finer('Parameter is mapped to $mappedParameterName');
+
+        if (parameter.parameterKind == ParameterKind.NAMED) {
+          _logger.finer('Parameter is named');
           namedArguments[new Symbol(parameterName)] = parameterValue;
         } else {
+          _logger.finer('Parameter is positional');
           positionalArguments.add(parameterValue);
         }
       }
 
       value = createAnnotation(
           clazz,
-          new Symbol(''),
+          new Symbol(representation.name),
           positionalArguments,
           namedArguments
       );
+    } else if (representation is PropertyAccessorElement) {
+      _logger.finer('Annotation is a field');
     }
 
     return value;
@@ -157,3 +212,7 @@ ClassMirror _findClassMirror(String name) {
   return mirror;
 }
 
+/// Passes through the [parameter] name.
+///
+/// Used when a [ParameterNameMapper] is not provided.
+String _passThroughParameters(String _, String parameter) => parameter;
