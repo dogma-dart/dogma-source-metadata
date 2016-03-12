047e13107e0a5c5b69a27de8bbdbbe3e5a4e1313
Don <don.j.olmstead@gmail.com>
2016-03-11 22:01:45 -0800

    Add mirrors test library

---
+++ test/lib/mirrors.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library dogma_source_analyzer.test.lib.mirrors;
+
+class A {
+  final int value;
+
+  A(this.value);
+
+  A._()
+     : value = defaultValue;
+
+  static int defaultValue = 2;
+}
+
+void function() { print('Im a function'); }
