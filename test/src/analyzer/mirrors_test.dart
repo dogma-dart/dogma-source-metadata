e88d2ab5dbbea118d8b9d2e4c761463b2839cba4
Don <don.j.olmstead@gmail.com>
2016-03-11 22:01:54 -0800

    Add mirrors test

---
+++ test/src/analyzer/mirrors_test.dart
@@ -0,0 +1,64 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/analyzer.dart';
+
+import '../../lib/mirrors.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+const String _class = 'A';
+const String _library = 'dogma_source_analyzer.test.lib.mirrors';
+const String _function = 'function';
+const String _notFound = 'NotFound';
+
+void main() {
+  test('classMirror tests', () {
+    var mirror;
+
+    mirror = classMirror(_class);
+    expect(mirror, isNotNull);
+
+    mirror = classMirror(_class, _library);
+    expect(mirror, isNotNull);
+
+    // Expect throws
+    expect(() => classMirror(_function), throwsArgumentError);
+    expect(() => classMirror(_function, _library), throwsArgumentError);
+
+    expect(() => classMirror(_notFound), throwsArgumentError);
+    expect(() => classMirror(_notFound, _library), throwsArgumentError);
+  });
+  test('createAnnotation', () {
+    var mirror = classMirror(_class);
+    expect(mirror, isNotNull);
+
+    var number = 5;
+
+    var instance = createAnnotation(mirror, new Symbol(''), [number], {});
+    expect(instance is A, isTrue);
+    expect(instance.value, equals(number));
+  });
+  test('privateConstructor', () {
+    var mirror = classMirror(_class);
+    expect(mirror, isNotNull);
+
+    var symbol = privateConstructor(mirror, new Symbol('_'));
+    expect(symbol, isNotNull);
+
+    var instance = createAnnotation(mirror, symbol, [], {});
+    expect(instance, isNotNull);
+    expect(instance is A, isTrue);
+    expect(instance.value, equals(A.defaultValue));
+  });
+}
