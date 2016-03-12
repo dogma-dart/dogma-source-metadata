ac70d470e5fd5b1e1934705e94d9cb0803ae7dd7
Don <don.j.olmstead@gmail.com>
2016-03-11 22:00:21 -0800

    Add way to override behavior when creating a const object

--- lib/src/analyzer/constant_object.dart
+++ lib/src/analyzer/constant_object.dart
@@ -8,25 +8,36 @@
 //---------------------------------------------------------------------
 
 import 'package:analyzer/src/generated/constant.dart';
+import 'package:analyzer/src/generated/element.dart';
 
 //---------------------------------------------------------------------
 // Library contents
 //---------------------------------------------------------------------
 
+/// A function that instantiates an instance of a dart object from the given
+/// [value].
+///
+///
+typedef dynamic CreateDartValue(DartObject value);
+
 /// Attempts to convert the [value] into a Dart object.
 ///
 /// This can be used to determine constant values that the analyzer can
 /// instantiate properly.
-dynamic dartValue(DartObjectImpl value) {
+dynamic dartValue(DartObject value,
+                 [CreateDartValue valueCreator]) {
+  // No creator was supplied so use the _unknownDartValue function
+  valueCreator ??= _unknownDartValue;
+
   var typeName = value.type.displayName;
 
   switch (typeName) {
     case 'String':
       return value.toStringValue();
     case 'Map':
-      return _toMapValue(value);
+      return _toMapValue(value, valueCreator);
     case 'List':
-      return _toListValue(value);
+      return _toListValue(value, valueCreator);
     case 'int':
       return value.toIntValue();
     case 'double':
@@ -41,21 +52,44 @@ dynamic dartValue(DartObjectImpl value) {
     case 'Null':
       return null;
     default:
-      assert(false);
-      return null;
+      return valueCreator(value);
   }
 }
 
+dynamic dartEnumIndex(DartObject value) {
+  var element = value.type.element;
+
+  return ((element is ClassElement) && (element.isEnum))
+      ? value.getField('index').toIntValue()
+      : null;
+}
+
+dynamic dartFunctionName(DartObject value) {
+  var element = value.type.element;
+
+  return element is FunctionElement ? element.name : null;
+}
+
+CreateDartValue joinDartValue(CreateDartValue a, CreateDartValue b) =>
+    (DartObject value) => a(value) ?? b(value);
+
+dynamic _unknownDartValue(DartObject _) {
+  assert(false);
+  return null;
+}
+
 /// Converts the [value] into a Dart List instance.
-List _toListValue(DartObjectImpl value) =>
-    value.toListValue().map((dartObject) => dartValue(dartObject)).toList();
+List _toListValue(DartObjectImpl value, CreateDartValue valueCreator) =>
+    value.toListValue().map(
+        (dartObject) => dartValue(dartObject, valueCreator)
+    ).toList();
 
 /// Converts the [value] into a Dart Map instance.
-Map _toMapValue(DartObjectImpl value) {
+Map _toMapValue(DartObjectImpl value, CreateDartValue valueCreator) {
   var map = {};
 
   value.toMapValue().forEach((key, value) {
-    map[dartValue(key)] = dartValue(value);
+    map[dartValue(key)] = dartValue(value, valueCreator);
   });
 
   return map;
