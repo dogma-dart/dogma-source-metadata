c9c0c77a373d9debc7c54631fbf2798858ee608a
Don <don.j.olmstead@gmail.com>
2016-04-05 22:51:15 -0700

    Add path tests

---
+++ test/path.dart
@@ -0,0 +1,84 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+import 'package:dogma_source_analyzer/path.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Entry point for tests.
+void main() {
+  test('join', () {
+    var result;
+
+    // Join file path
+    result = join('a/b.dart');
+    expect(result.scheme, 'file');
+    expect(result.path.endsWith('a/b.dart'), isTrue);
+
+    // Join package path
+    result = join('src/a/b.dart', base: Uri.parse('package:foo'));
+    expect(result.scheme, 'package');
+    expect(result, Uri.parse('package:foo/src/a/b.dart'));
+
+    // Test normalization of base
+    result = join('src/a/b.dart', base: Uri.parse('package:foo/a/../'));
+    expect(result.scheme, 'package');
+    expect(result, Uri.parse('package:foo/src/a/b.dart'));
+
+    // Test normalization of value
+    result = join('src/a/../a/../a/b.dart', base: Uri.parse('package:foo'));
+    expect(result.scheme, 'package');
+    expect(result, Uri.parse('package:foo/src/a/b.dart'));
+  });
+  test('relative', () {
+    expect(true, true);
+  });
+  test('dirname', () {
+    // Strings
+    expect(dirname('path/to/foo.dart'), 'path/to');
+    expect(dirname('path/to'), 'path');
+    expect(dirname('path/to/'), 'path');
+    expect(dirname('path/to/../to/foo.dart'), 'path/to');
+
+    // Uris
+    expect(dirname(Uri.parse('package:foo/to/foo.dart')), 'foo/to');
+    expect(dirname(Uri.parse('package:foo/to')), 'foo');
+    expect(dirname(Uri.parse('package:foo/to/')), 'foo');
+    expect(dirname(Uri.parse('package:foo/to/../to/foo.dart')), 'foo/to');
+  });
+  test('basename', () {
+    // Strings
+    expect(basename('path/to/foo.dart'), 'foo.dart');
+    expect(basename('path/to'), 'to');
+    expect(basename('path/to/'), 'to');
+    expect(basename('path/to/../to/foo.dart'), 'foo.dart');
+
+    // Uris
+    expect(basename(Uri.parse('package:foo/to/foo.dart')), 'foo.dart');
+    expect(basename(Uri.parse('package:foo/to')), 'to');
+    expect(basename(Uri.parse('package:foo/to/')), 'to');
+    expect(basename(Uri.parse('package:foo/to/../to/foo.dart')), 'foo.dart');
+  });
+  test('basenameWithoutExtension', () {
+    // Strings
+    expect(basenameWithoutExtension('path/to/foo.dart'), 'foo');
+    expect(basenameWithoutExtension('path/to'), 'to');
+    expect(basenameWithoutExtension('path/to/'), 'to');
+    expect(basenameWithoutExtension('path/to/../to/foo.dart'), 'foo');
+
+    // Uris
+    expect(basenameWithoutExtension(Uri.parse('package:foo/to/foo.dart')), 'foo');
+    expect(basenameWithoutExtension(Uri.parse('package:foo/to')), 'to');
+    expect(basenameWithoutExtension(Uri.parse('package:foo/to/')), 'to');
+    expect(basenameWithoutExtension(Uri.parse('package:foo/to/../to/foo.dart')), 'foo');
+  });
+}
