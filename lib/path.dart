a57be1b969ce9958aec43f91073bd06bbc8bb11f
Don <don.j.olmstead@gmail.com>
2016-04-05 22:50:57 -0700

    Handle package uris as well as files

--- lib/path.dart
+++ lib/path.dart
@@ -4,6 +4,10 @@
 // the LICENSE file.
 
 /// Functions for handling uri paths.
+///
+/// The majority of the functions are wrappers around functionality found in
+/// the `path` library. They are expanded to support Uri values which are used
+/// heavily within the analyzer to get paths to resources.
 library dogma_source_analyzer.path;
 
 //---------------------------------------------------------------------
@@ -16,6 +20,10 @@ import 'package:path/path.dart' as p;
 // Library contents
 //---------------------------------------------------------------------
 
+/// The path to the current working directory expressed as a Uri.
+///
+/// It is assumed that the current path is within the root of a package. If
+/// that is not the case then this value will be incorrect.
 final Uri currentPathUri = p.toUri(p.current);
 
 /// The current path running the application.
@@ -26,104 +34,150 @@ final Uri currentPathUri = p.toUri(p.current);
 /// implementation held in path.
 final String currentPath = currentPathUri.toFilePath(windows: false);
 
-Uri join(dynamic value, {dynamic base}) {
+/// Joins the [path] into the [base].
+///
+/// If [base] is not specified then the [currentPathUri] is used instead.
+Uri join(String path, {dynamic base}) {
+  // Default to the current path
   base ??= currentPath;
 
-  if (base is Uri) {
-    base = base.toFilePath(windows: false);
-  }
+  // Get the base path and scheme
+  var basePath = _uriPath(base);
+  var baseScheme = _uriScheme(base);
 
-  if (value is String) {
-    value = p.posix.joinAll(p.split(value));
-  }
+  // Get the normalized value path
+  var toPath = _uriPath(path);
 
-  var joined = p.posix.join(base, value);
+  // Join the path
+  var joined = p.posix.normalize(p.posix.join(basePath, toPath));
 
-  return p.posix.toUri(joined);
+  // Create the URI
+  return new Uri(scheme: baseScheme, path: joined);
 }
 
-String relative(dynamic value, {dynamic from}) {
+/// Gets the relative path [from] the [value].
+///
+/// If [from] is not specified then [currentPathUri] is used.
+String relative(dynamic path, {dynamic from}) {
   from ??= currentPath;
 
-  // Turn into a file path
-  from = _filePath(from);
-  value = _filePath(value);
+  // Get the schemes
+  var pathScheme = _uriScheme(path);
+  var fromScheme = _uriScheme(from);
 
-  // Convert from into the dirname
-  from = _isDirectory(from) ? from : p.posix.dirname(from);
-
-  return p.posix.relative(value, from: from);
-}
-
-bool isWithin(dynamic parent, dynamic child) {
-  // Turn into a file path
-  parent = _filePath(parent);
-  child = _filePath(child);
-
-  return p.posix.isWithin(parent, child);
-}
-
-String dirname(dynamic value) {
-  value = _filePath(value);
-
-  return p.posix.dirname(value);
-}
-
-String basename(dynamic value) {
-  value = _filePath(value);
-
-  return p.posix.basename(value);
-}
-
-String basenameWithoutExtension(dynamic value) {
-  value = _filePath(value);
-
-  return p.posix.basenameWithoutExtension(value);
-}
-
-Uri libraryPath(String value) {
-  // Split based on the .
-  var split = value.split('.').sublist(1);
-
-  var base = _isInLib(split[0])
-      ? p.posix.join(currentPath, 'lib')
-      : currentPath;
-
-  // Add .dart to the last value
-  split[split.length - 1] += '.dart';
-
-  return p.posix.toUri(p.posix.join(base, p.posix.joinAll(split)));
-}
-
-String libraryName(String package, dynamic path) {
-  path = _filePath(path);
-
-  // Get the relative path
-  var relative = p.posix.relative(path, from: currentPath);
-  var withoutExtension = p.posix.withoutExtension(relative);
-
-  // Split the path
-  var split = p.posix.split(withoutExtension);
-
-  // Check if the value is in lib
-  if (_isInLib(split[0])) {
-    split = split.sublist(1);
+  if (pathScheme != fromScheme) {
+    throw new ArgumentError('The schemes of the values do not match');
   }
 
-  // Append the package name at the front
-  split = <String>[package]..addAll(split);
+  // Turn into a path
+  var fromPath = _uriPath(from);
+  var toPath = _uriPath(path);
 
-  return split.join('.');
+  // Convert from into the dirname
+  fromPath = _isDirectory(fromPath) ? fromPath : p.posix.dirname(fromPath);
+
+  // Get the relative path
+  return p.posix.relative(toPath, from: fromPath);
 }
 
-/// Converts the [value] into a file path.
-String _filePath(dynamic value) =>
-    value is Uri ? value.toFilePath(windows: false) : value;
+/// Returns `true` if [child] is a path beneath `parent`, and `false`
+/// otherwise.
+///
+///     isWithin('/root/path', '/root/path/a'); // -> true
+///     isWithin('/root/path', '/root/other'); // -> false
+///     isWithin('/root/path', '/root/path'); // -> false
+bool isWithin(dynamic parent, dynamic child) {
+  // Turn into a path
+  var parentPath = _uriPath(parent);
+  var childPath = _uriPath(child);
 
-bool _isInLib(String value) =>
-    value != 'example' &&
-    value != 'test' &&
-    value != 'bin' &&
-    value != 'tool';
+  return p.posix.isWithin(parentPath, childPath);
+}
 
+/// Gets the part of [path] before the last separator.
+///
+///     dirname('path/to/foo.dart'); // -> 'path/to'
+///     dirname('path/to');          // -> 'path'
+///
+/// Trailing separators are ignored.
+///
+///     dirname('path/to/'); // -> 'path'
+String dirname(dynamic path) => p.posix.dirname(_uriPath(path));
+
+/// Gets the part of [path] after the last separator.
+///
+///     basename('path/to/foo.dart');                    // -> 'foo.dart'
+///     basename('path/to');                             // -> 'to'
+///     basename(Uri.parse('package:path/to/foo.dart')); // -> 'foo.dart'
+///     basename(Uri.parse('package:path/to'));          // -> 'to'
+///
+/// Trailing separators are ignored.
+///
+///     basename('path/to/'); // -> 'to'
+String basename(dynamic value) => p.posix.basename(_uriPath(value));
+
+/// Gets
+String basenameWithoutExtension(dynamic value) =>
+    p.posix.basenameWithoutExtension(_uriPath(value));
+
+/// Checks whether the [path] can be imported through package notation.
+bool canImportAsPackage(dynamic path) {
+  var pathScheme = _uriScheme(path);
+
+  // See if its already a package path
+  if (pathScheme == 'package') {
+    return true;
+  }
+
+  var parentPath = _libPath();
+  var childPath = _uriPath(path);
+
+  return p.posix.isWithin(parentPath, childPath);
+}
+
+/// Converts the [path] to a package Uri with the given [name].
+///
+/// To verify that this function can be used [canImportAsPackage] should be
+/// called first.
+Uri asPackageImport(dynamic path, String name) {
+  var pathScheme = _uriPath(path);
+
+  // See if its already a package path
+  if (pathScheme == 'package') {
+    var uri = path as Uri;
+
+    if (uri.pathSegments[0] == name) {
+      return uri;
+    } else {
+      throw new ArgumentError.value(path, 'Is already a package but not the package specified');
+    }
+  }
+
+  var parentPath = _libPath();
+  var childPath = _uriPath(path);
+
+  if (!p.posix.isWithin(parentPath, childPath)) {
+    throw new ArgumentError.value(path, 'The path is not within the lib directory');
+  }
+
+  var relativePath = p.posix.relative(childPath, from: parentPath);
+
+  return new Uri(scheme: 'package', path: p.join(name, relativePath));
+}
+
+/// Gets the path of a package library from the current path.
+String _libPath() => p.posix.join(_uriPath(currentPathUri), 'lib');
+
+/// Returns the normalized path of the value.
+String _uriPath(dynamic value) =>
+    (value is Uri)
+        ? value.normalizePath().path
+        : p.posix.normalize(p.posix.joinAll(p.split(value)));
+
+/// Returns the scheme of a URI.
+///
+/// If the value is not a URI then it is assumed to be a file uri.
+String _uriScheme(dynamic value) => (value is Uri) ? value.scheme : 'file';
+
+/// Checks to see if the [value] is a directory.
 bool _isDirectory(String value) => p.posix.withoutExtension(value) == value;
