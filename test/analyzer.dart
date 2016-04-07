ae5ad9b55deff46bf672270ee1b10609681ba812
Don <don.j.olmstead@gmail.com>
2016-04-06 23:59:58 -0700

    Add additional analyzer tests into the runner

--- test/analyzer.dart
+++ test/analyzer.dart
@@ -14,9 +14,12 @@ import 'package:test/test.dart';
 import 'src/analyzer/annotation_test.dart' as annotation_test;
 import 'src/analyzer/class_metadata_test.dart' as class_metadata_test;
 import 'src/analyzer/constructor_metadata_test.dart' as constructor_metadata_test;
+import 'src/analyzer/deprecated_annotation_test.dart' as deprecated_metadata_test;
+import 'src/analyzer/enum_metadata_test.dart' as enum_metadata_test;
 import 'src/analyzer/field_metadata_test.dart' as field_metadata_test;
 import 'src/analyzer/function_metadata_test.dart' as function_metadata_test;
 import 'src/analyzer/mirrors_test.dart' as mirrors_test;
+import 'src/analyzer/protected_annotation_test.dart' as protected_annotation_test;
 import 'src/analyzer/type_metadata_test.dart' as type_metadata_test;
 
 //---------------------------------------------------------------------
@@ -28,8 +31,11 @@ void main() {
   group('Annotation', annotation_test.main);
   group('ClassMetadata', class_metadata_test.main);
   group('ConstructorMetadata', constructor_metadata_test.main);
+  group('EnumMetadata', enum_metadata_test.main);
+  group('Deprecated', deprecated_metadata_test.main);
   group('FieldMetadata', field_metadata_test.main);
   group('FunctionMetadata', function_metadata_test.main);
   group('Mirrors', mirrors_test.main);
+  group('Protected', protected_annotation_test.main);
   group('TypeMetadata', type_metadata_test.main);
 }
