b1d53be5d0882ca46b7b57c3da17733926e984b5
Don <don.j.olmstead@gmail.com>
2016-10-23 09:23:03 -0700

    Add abstract metadata builder

---
+++ lib/src/metadata_builder/abstract_metadata_builder.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Values for building abstract metadata.
+///
+/// [AbstractMetadataBuilder] should be mixed into the MetadataBuilder where
+/// applicable.
+class AbstractMetadataBuilder {
+  /// Whether the metadata is abstract.
+  bool isAbstract = false;
+}
