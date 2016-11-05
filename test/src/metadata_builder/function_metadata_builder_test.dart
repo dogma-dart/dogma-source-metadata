35919813877208a0ba27ef88d46e026c05befd75
Don <don.j.olmstead@gmail.com>
2016-11-05 08:29:12 -0700

    Add function metadata tests

---
+++ test/src/metadata_builder/function_metadata_builder_test.dart
@@ -0,0 +1,70 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_metadata_test/test.dart';
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/metadata.dart';
+import 'package:dogma_source_analyzer/metadata_builder.dart';
+
+import 'expect_metadata.dart';
+import 'function_metadata_validate.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+const String _name = 'function';
+
+/// Test entry point.
+void main() {
+  test('validate parameter names', expectThrowsOnRepeatedNames(function(_name)));
+  test('validate parameter order', expectThrowsOnInvalidPosition(function(_name)));
+  test('validate optional and named', expectThrowsOnPositionalAndNamed(function(_name)));
+  test('validate initializers', expectThrowsOnInitializers(function(_name)));
+  test('builder defaults', () {
+    final builder = new FunctionMetadataBuilder()
+        ..name = _name;
+    final expected = new FunctionMetadata(_name);
+
+    expectMetadataEqual/*<FunctionMetadata>*/(builder, expected);
+    expectMetadataEqual/*<FunctionMetadata>*/(function(_name), expected);
+  });
+  test('values set', () {
+    final returnType = intType;
+    final paramName = 'param';
+    final annotations = [deprecated];
+    final comments = 'comment';
+
+    final builder = new FunctionMetadataBuilder()
+        ..name = _name
+        ..returnType = returnType
+        ..parameters = <ParameterMetadataBuilder>[
+          parameter(paramName)
+        ]
+        ..annotations = annotations
+        ..comments = comments;
+    final expected = new FunctionMetadata(
+        _name,
+        returnType: returnType,
+        parameters: <ParameterMetadata>[
+          new ParameterMetadata(paramName)
+        ]);
+
+
+    expectMetadataEqual/*<FunctionMetadata>*/(builder, expected);
+
+    // Make sure annotated metadata is present
+    final actual = builder.build();
+
+    expect(actual.annotations, hasLength(1));
+    expect(actual, isDeprecated);
+    expect(actual, commentedWith(comments));
+  });
+}
---
+++ test/src/metadata_builder/function_metadata_validate.dart
@@ -0,0 +1,67 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_analyzer/metadata_builder.dart';
+
+import 'invalid_metadata_error.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+expectThrowsOnRepeatedNames(FunctionMetadataBuilder builder) => () {
+  builder
+      ..parameters = [
+        parameter('foo'),
+        parameter('foo')
+      ];
+
+  expectThrowsInvalidMetadataError(builder);
+};
+
+expectThrowsOnInvalidPosition(FunctionMetadataBuilder builder) => () {
+  builder
+      ..parameters = [
+        parameter('_0'),
+        positionalParameter('_1'),
+        parameter('_2')
+      ];
+
+  expectThrowsInvalidMetadataError(builder);
+
+  builder
+     ..parameters = <ParameterMetadataBuilder>[
+       parameter('_0'),
+       parameter('_1'),
+       namedParameter('_2'),
+       parameter('_3')
+     ];
+
+  expectThrowsInvalidMetadataError(builder);
+};
+
+expectThrowsOnPositionalAndNamed(FunctionMetadataBuilder builder) => () {
+  builder
+      ..parameters = [
+        parameter('_0'),
+        positionalParameter('_1'),
+        namedParameter('_2')
+      ];
+
+  expectThrowsInvalidMetadataError(builder);
+};
+
+expectThrowsOnInitializers(FunctionMetadataBuilder builder) => () {
+  builder
+      ..parameters = [
+        parameter('foo')..isInitializer = true
+      ];
+
+  expectThrowsInvalidMetadataError(builder);
+};
