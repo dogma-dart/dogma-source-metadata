9a581cb2c7a02e629702048de9e663355c10220e
Don <don.j.olmstead@gmail.com>
2016-11-05 08:29:28 -0700

    Add constructor metadata tests

---
+++ test/src/metadata_builder/constructor_metadata_builder_test.dart
@@ -0,0 +1,49 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/metadata.dart';
+import 'package:dogma_source_analyzer/metadata_builder.dart';
+
+import 'expect_metadata.dart';
+import 'function_metadata_validate.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Test entry point.
+void main() {
+  test('validate parameter names', expectThrowsOnRepeatedNames(defaultConstructor()));
+  test('validate parameter order', expectThrowsOnInvalidPosition(defaultConstructor()));
+  test('validate optional and named', expectThrowsOnPositionalAndNamed(defaultConstructor()));
+  test('validate initializers', expectThrowsOnInitializers(defaultConstructor()..isFactory = true));
+  test('builder defaults', () {
+    final returnType = new TypeMetadata('Foo');
+    final builder = new ConstructorMetadataBuilder()
+        ..returnType = returnType;
+    final expected = new ConstructorMetadata(returnType);
+
+    expectMetadataEqual/*<ConstructorMetadata>*/(builder, expected);
+    expectMetadataEqual/*<ConstructorMetadata>*/(defaultConstructor()..returnType = returnType, expected);
+  });
+  test('initializers', () {
+    final builder = defaultConstructor()
+        ..parameters = <ParameterMetadataBuilder>[
+          parameter('test')
+              ..isInitializer = true
+        ];
+    final built = builder.build();
+    expect(built.parameters, hasLength(1));
+
+    final param = built.parameters[0];
+    expect(param.isInitializer, isTrue);
+  });
+}
