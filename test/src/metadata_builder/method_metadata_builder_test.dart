31c9b4bbb89ca13d1c8c6d4fd8ea1c91026e3978
Don <don.j.olmstead@gmail.com>
2016-11-05 08:29:20 -0700

    Add method metadata tests

---
+++ test/src/metadata_builder/method_metadata_builder_test.dart
@@ -0,0 +1,78 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_metadata_test/test.dart';
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/metadata.dart';
+import 'package:dogma_source_analyzer/metadata_builder.dart';
+
+import 'expect_metadata.dart';
+import 'invalid_metadata_error.dart';
+import 'function_metadata_validate.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+const String _name = 'function';
+
+/// Test entry point.
+void main() {
+  test('validate parameter names', expectThrowsOnRepeatedNames(method(_name)));
+  test('validate parameter order', expectThrowsOnInvalidPosition(method(_name)));
+  test('validate optional and named', expectThrowsOnPositionalAndNamed(method(_name)));
+  test('validate initializers', expectThrowsOnInitializers(method(_name)));
+  test('validate static/abstract', () {
+    final builder = new MethodMetadataBuilder()
+        ..name = _name
+        ..isAbstract = true
+        ..isStatic = true;
+
+    expectThrowsInvalidMetadataError(builder);
+  });
+  test('builder defaults', () {
+    final builder = new MethodMetadataBuilder()
+        ..name = _name;
+    final expected = new MethodMetadata(_name);
+
+    expectMetadataEqual/*<MethodMetadata>*/(builder, expected);
+    expectMetadataEqual/*<MethodMetadata>*/(method(_name), expected);
+  });
+  test('values set', () {
+    final returnType = intType;
+    final paramName = 'param';
+    final annotations = [deprecated];
+    final comments = 'comment';
+
+    final builder = new FunctionMetadataBuilder()
+      ..name = _name
+      ..returnType = returnType
+      ..parameters = <ParameterMetadataBuilder>[
+        parameter(paramName)
+      ]
+      ..annotations = annotations
+      ..comments = comments;
+    final expected = new FunctionMetadata(
+        _name,
+        returnType: returnType,
+        parameters: <ParameterMetadata>[
+          new ParameterMetadata(paramName)
+        ]);
+
+    expectMetadataEqual/*<FunctionMetadata>*/(builder, expected);
+
+    // Make sure annotated metadata is present
+    final actual = builder.build();
+
+    expect(actual.annotations, hasLength(1));
+    expect(actual, isDeprecated);
+    expect(actual, commentedWith(comments));
+  });
+}
