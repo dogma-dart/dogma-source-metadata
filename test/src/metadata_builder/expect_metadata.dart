4542ee418e35f3386feb49057a949afc3aaf7b25
Don <don.j.olmstead@gmail.com>
2016-11-05 08:28:45 -0700

    Add test for parameter metadata builder

---
+++ test/src/metadata_builder/expect_metadata.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_metadata_test/test.dart';
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/metadata.dart';
+import 'package:dogma_source_analyzer/metadata_builder.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+void expectMetadataEqual/*<T extends Metadata>*/(MetadataBuilder/*<T>*/ actual,
+                                                 Metadata/*=T*/ expected) {
+  expect(actual.build(), metadataEqual/*<T>*/(expected));
+}
---
+++ test/src/metadata_builder/parameter_metadata_builder_test.dart
@@ -0,0 +1,100 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:dogma_source_metadata_test/test.dart';
+import 'package:test/test.dart';
+
+import 'package:dogma_source_analyzer/metadata.dart';
+import 'package:dogma_source_analyzer/metadata_builder.dart';
+
+import 'expect_metadata.dart';
+import 'invalid_metadata_error.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+const String _name = 'test';
+
+/// Test entry point.
+void main() {
+  test('validate required no default', () {
+    final builder = parameter(_name)
+      ..defaultValue = 5;
+
+    expectThrowsInvalidMetadataError(builder);
+  });
+  test('builder defaults', () {
+    final builder = new ParameterMetadataBuilder()
+        ..name = _name;
+    final expected = new ParameterMetadata(_name);
+
+    expectMetadataEqual/*<ParameterMetadata>*/(builder, expected);
+    expectMetadataEqual/*<ParameterMetadata>*/(parameter(_name), expected);
+  });
+  test('values set', () {
+    final parameterKind = ParameterKind.positional;
+    final type = intType;
+    final isInitializer = true;
+    final defaultValue = 5;
+    final annotations = [deprecated];
+
+    final builder = new ParameterMetadataBuilder()
+        ..name = _name
+        ..type = type
+        ..parameterKind = parameterKind
+        ..isInitializer = isInitializer
+        ..defaultValue = defaultValue
+        ..annotations = annotations;
+    final expected = new ParameterMetadata(
+        _name,
+        type: type,
+        parameterKind: parameterKind,
+        isInitializer: isInitializer,
+        defaultValue: defaultValue
+    );
+
+    expectMetadataEqual/*<ParameterMetadata>*/(builder, expected);
+
+    // Make sure annotated metadata is present
+    //
+    // Comments are not valid on parameters
+    final actual = builder.build();
+
+    expect(actual.annotations, hasLength(1));
+    expect(actual, isDeprecated);
+  });
+  test('parameter', () {
+    final builder = parameter(_name);
+    final expected = new ParameterMetadata(
+        _name,
+        parameterKind: ParameterKind.required
+    );
+
+    expectMetadataEqual/*<ParameterMetadata>*/(builder, expected);
+  });
+  test('positionalParameter', () {
+    final builder = positionalParameter(_name);
+    final expected = new ParameterMetadata(
+        _name,
+        parameterKind: ParameterKind.positional
+    );
+
+    expectMetadataEqual/*<ParameterMetadata>*/(builder, expected);
+  });
+  test('namedParameter', () {
+    final builder = namedParameter(_name);
+    final expected = new ParameterMetadata(
+        _name,
+        parameterKind: ParameterKind.named
+    );
+
+    expectMetadataEqual/*<ParameterMetadata>*/(builder, expected);
+  });
+}
