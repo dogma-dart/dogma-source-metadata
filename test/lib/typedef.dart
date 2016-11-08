d7ddd84f375adef2e71832e2641ac2281fa70f42
Don <don.j.olmstead@gmail.com>
2016-11-07 21:19:48 -0800

    Add typedef test file

---
+++ test/lib/typedef.dart
@@ -0,0 +1,8 @@
+// Copyright (c) 2015-2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+typedef void EmptyFunction();
+typedef String ParamFunction(String a, String b);
+typedef T GenericFunction<T>(T param);
